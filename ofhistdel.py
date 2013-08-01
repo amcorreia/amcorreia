aa1804e7a889c69e91ea41bfa77feaec5c50f5da
Alessandro Madruga Correia <mutley.sandro@gmail.com>
2013-08-01 18:15:49 -0300

    Now is possible to list messages from database

--- ofhistdel.py
+++ ofhistdel.py
@@ -1,10 +1,43 @@
 #!/usr/bin/env python
-# Copyright (c) 2011 - Alessandro Madruga Correia <amcorreia@zarathustra.com.br>
-
+'''
+   Copyright (c) 2011-2013 - Alessandro Madruga Correia <amcorreia@zarathustra.com.br>
+  	The Regents of the University of California.  All rights reserved.
+  
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+   1. Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+   3. All advertising materials mentioning features or use of this software
+      must display the following acknowledgement:
+  	This product includes software developed by the University of
+  	California, Berkeley and its contributors.
+   4. Neither the name of the University nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
+  
+   THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+   OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+   OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+   SUCH DAMAGE.
+'''
+ 
 # Utilizado para apagar todo o historico de conversas do JID fornecido
 # plugin do openfire: "Monitoring Service"
 
+
 import sys
+import time
 
 try:
 	import adodb
@@ -21,10 +54,14 @@ __prog_name = None
 
 
 def help ():
-    print '''
-    Uso: %s <JID>
-    Apaga TODO o historico de conversas do <JID>
-    ''' % __prog_name
+    print '''Usage: %s COMMAND [ DATE ]
+Where COMMAND = { l[ist] | d[elete] }
+      DATE    = [ date_begin DATE_VALUE | date_end DATE_VALUE ]
+      DATE_VALUE = [ dd/mm/yy | dd/mm/yy HH:MM ]
+
+    list JID      Lista o historico de conversas do JID
+    delete JID    Deleta o historico de conversas do JID
+    ''' %  __prog_name
     sys.exit (0)
 
 # eh tiro curto
@@ -72,10 +109,58 @@ def del_all_msg (jid):
         perc = (((idx+1)*100) / count)
         update_progress (perc)
     print ''
-	
-def main (argv):
-    global conn
-    jid = argv[0]
+
+def parse_date (argv):
+    tm_start = 0
+    tm_stop = 0
+    idx = 0
+
+    while idx <= (len(argv)-1):
+        if argv[idx] == 'date_begin':
+            tm_start = argv[idx+1]
+        if argv[idx] == 'date_end':
+            tm_stop = argv[idx+1]
+        idx = idx + 1
+ 
+    #print 'list %s I %s F %s' % (jid,tm_start, tm_stop)
+    if tm_start == 0:
+        tm_start = int(time.mktime(time.strptime("01/01/1970 00:00", "%d/%m/%Y %H:%M"))) * 1000;
+    else:
+        try:
+            tm_start = int(time.mktime(time.strptime(tm_start, "%d/%m/%Y %H:%M"))) * 1000;
+        except:
+            tm_start = int(time.mktime(time.strptime(tm_start, "%d/%m/%Y"))) * 1000;
+ 
+    if tm_stop == 0:
+        tm_stop = int(time.time()*1000)
+    else:
+        try:
+            tm_stop  = int(time.mktime(time.strptime(tm_stop, "%d/%m/%Y %H:%M"))) * 1000;
+        except:
+            tm_stop  = int(time.mktime(time.strptime(tm_stop, "%d/%m/%Y"))) * 1000;
+    return { 'tm_start' : tm_start, 'tm_stop' : tm_stop }
+ 
+ 
+# COMMANDS
+def cmd_list (argv):
+    jid = argv.pop(0)
+    tm = parse_date (argv)
+    tm_start = tm['tm_start']
+    tm_stop  = tm['tm_stop']
+    conn = adodb.NewADOConnection (conn_string)
+ 
+    sql = '''SELECT * FROM ofmessagearchive WHERE sentdate > %d and sentdate < %d and (fromjid like '%%%s%%' or tojid like '%%%s%%') ORDER BY sentdate''' % (tm_start, tm_stop, jid, jid)
+    cu  = conn.Execute (sql)
+    while not cu.EOF:
+        arr = cu.GetRowAssoc(0) # 0 is lower, 1 is upper-case
+        print '%s %s -> %s:  %s' % ( time.strftime("%d/%m/%Y %H:%M:%S",time.localtime(int(arr['sentdate']/1000))) , arr['fromjid'].split('/')[0], arr['tojid'].split('/')[0], arr['body'])
+        cu.MoveNext()
+    conn.Close ()
+    #print 'list %s I %s F %s XXXX %s %s' % (jid,tm_start, tm_stop, T['tm_start'], T['tm_stop'])
+
+
+def cmd_delete (argv):
+    jid = argv[1]
     try:
         jid.index ('@')
     except ValueError:
@@ -86,9 +171,26 @@ def main (argv):
     conn.Close ()
 
 
+def main (argv):
+    global conn
+    cmd = argv.pop(0)
+    if len (argv) < 1:
+        print 'Faltam parametros'
+        sys.exit (-1)
+
+    if cmd in ['list', 'l']:
+       cmd_list (argv)
+
+    elif cmd in ['delete', 'd']:
+       cmd_delete (argv)
+
+    else:
+       help ()
+
+
 if '__main__' == __name__:
-    if len (sys.argv) < 2 or sys.argv[1] == '-h':
-        help ()
     __prog_name = sys.argv.pop(0)
+    if len (sys.argv) < 1 or sys.argv[0] in ['-h', 'h', 'help']:
+        help ()
     main (sys.argv)
 
